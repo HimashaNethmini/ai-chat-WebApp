9cd2b7f94e8c98d4cf0249cb3cfa90ecdbc5ab2c
HimashaNethmini <himashanethmini05@gmail.com>
2024-07-28 17:00:30 +0530

    enabling client side routing

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,31 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {  createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Homepage from './routes/homepage/Homepage.jsx'
+import Dashboard from './routes/dashboard/Dashboard.jsx'
+import ChatPage from './routes/chatPage/ChatPage.jsx'
+
+/*enabling client side routing */
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <Homepage />
+    ),
+  },
+  {
+    path: "/dashboard",
+    children: [
+      { path: "/dashboard", element: <Dashboard /> },
+      { path: "/dashboard/chats/:id", element: <ChatPage/>},
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
+
