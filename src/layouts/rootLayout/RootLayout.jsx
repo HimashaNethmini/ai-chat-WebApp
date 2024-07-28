12afd6b140e623734a0081382935d0cf6d5b9fcd
HimashaNethmini <himashanethmini05@gmail.com>
2024-07-28 17:39:08 +0530

    added styles on layout page

--- src/layouts/rootLayout/RootLayout.jsx
+++ src/layouts/rootLayout/RootLayout.jsx
@@ -1,17 +1,19 @@
-import React from "react";
+import './rootLayout.css'
 import { Link } from "react-router-dom";
+import { Outlet } from 'react-router-dom';
 
 // enabling client side routing
 const RootLayout = () => {
   return (
-    <div className="RootLayout">
+    <div className="rootLayout">
       <header>
-        <Link to="/" className="logo">
+        <Link to="/">
           <img src="/logo.png" alt="" />
           <span>LAMA AI</span>
         </Link>
-      </header>
-      
+        <div className="user">User</div>
+
+      </header>  
       <main>
         <Outlet />
       </main>
