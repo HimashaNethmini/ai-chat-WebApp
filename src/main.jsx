476c7cf311c4a1074de1eaf63451ae5620894797
HimashaNethmini <himashanethmini05@gmail.com>
2024-07-28 17:38:50 +0530

    imported components

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import {  createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Homepage from './routes/homepage/Homepage.jsx'
+import React from "react";
+import "./index.css";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import RootLayout from "./layouts/rootLayout/RootLayout.jsx";
+import Homepage from "./routes/homepage/Homepage.jsx";
+import DashboardLayout from "./layouts/dashboardLayout/DashboardLayout.jsx";
+import Dashboard from "./routes/dashboard/Dashboard.jsx"
+import ChatPage from "./routes/chatPage/ChatPage.jsx";
 
-/*enabling client side routing */
+/*enabling client side routing using layout*/
 const router = createBrowserRouter([
   {
     element: <RootLayout />,
@@ -14,13 +17,25 @@ const router = createBrowserRouter([
         path: "/",
         element: <Homepage />,
       },
+      {
+        element: <DashboardLayout />,
+        children: [
+          {
+            path: "/dashboard",
+            element: <Dashboard />,
+          },
+          {
+            path: "/dashboard/chats/:id",
+            element: <ChatPage />,
+          },
+        ],
+      },
     ],
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
-
+  </React.StrictMode>
+);
