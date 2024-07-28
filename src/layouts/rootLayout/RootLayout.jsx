8483affcfc5e21433ab301fdf3e56f2c57bcda94
HimashaNethmini <himashanethmini05@gmail.com>
2024-07-28 18:49:06 +0530

    wrap app with ClerkProvider

--- src/layouts/rootLayout/RootLayout.jsx
+++ src/layouts/rootLayout/RootLayout.jsx
@@ -1,23 +1,40 @@
 import './rootLayout.css'
 import { Link } from "react-router-dom";
 import { Outlet } from 'react-router-dom';
+import { ClerkProvider, SignedIn, UserButton } from '@clerk/clerk-react'
+
+// Import the publishable key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
 
 // enabling client side routing
 const RootLayout = () => {
   return (
-    <div className="rootLayout">
-      <header>
-        <Link to="/">
-          <img src="/logo.png" alt="" />
-          <span>LAMA AI</span>
-        </Link>
-        <div className="user">User</div>
 
-      </header>  
-      <main>
-        <Outlet />
-      </main>
-    </div>
+    //add <ClerkProvider> to my app
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <div className="rootLayout">
+        <header>
+          <Link to="/" className='logo'>
+            <img src="/logo.png" alt="" />
+            <span>LAMA AI</span>
+          </Link>
+
+          <div className="user">
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </div>
+        </header>  
+
+        <main>
+          <Outlet />
+        </main>
+      </div>
+    </ClerkProvider>
   );
 };
 
