ccae346051376240825f2b0aebc6de3f1500477f
HimashaNethmini <himashanethmini05@gmail.com>
2024-07-30 22:30:32 +0530

    dashboard is only shown if the user is sign-in

--- src/layouts/dashboardLayout/DashboardLayout.jsx
+++ src/layouts/dashboardLayout/DashboardLayout.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './dashboardLayout.css'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
+import { useAuth } from '@clerk/clerk-react'
 
 const DashboardLayout = () => {
+
+  const { userId, isLoaded } = useAuth();
+  const navigate = useNavigate();
+
+  //loaded but no user id - redirect to sign in page
+  useEffect(() => {
+    if ( isLoaded && !userId ){
+        navigate("/sign-in");
+    }
+  }, [isLoaded, userId, navigate]);
+
+  if (!isLoaded) return "Loading...";
+  
   return (
     <div className='DashboardLayout'>
       <div className="menu">MENU</div>
