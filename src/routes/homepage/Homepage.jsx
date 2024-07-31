164ead3dcbf132e87d9fbe48d971cf7b0a2d08aa
HimashaNethmini <himashanethmini05@gmail.com>
2024-07-31 11:28:19 +0530

    left side - homepage

--- src/routes/homepage/Homepage.jsx
+++ src/routes/homepage/Homepage.jsx
@@ -2,7 +2,21 @@ import './homepage.css'
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+    <div className='homepage'>
+
+      {/* left side - homepage */}
+      <div className="left">
+        <h1>LAMA AI</h1>
+        <h2>Supercharge your creativity and productivity</h2>
+        <h3>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+        </h3>
+        <button>Get Started</button>
+      </div>
+
+      {/* right side - homepage */}
+      <div className="right"></div>
+    </div>
   )
 }
 
