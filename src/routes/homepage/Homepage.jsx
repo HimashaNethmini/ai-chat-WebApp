8c7d5c69d89e90595f5f74f005228f18c96b5fde
HimashaNethmini <himashanethmini05@gmail.com>
2024-07-31 16:29:13 +0530

    homepage completed

--- src/routes/homepage/Homepage.jsx
+++ src/routes/homepage/Homepage.jsx
@@ -1,7 +1,10 @@
 import './homepage.css'
 import { Link } from 'react-router-dom'
+import { TypeAnimation } from "react-type-animation";
+import { useState } from 'react';
 
 const Homepage = () => {
+  const [typingStatus, setTypingStatus] = useState("human1");
   return (
     <div className='homepage'>
       <img src="/orbital.png" alt="" className='orbital'/>
@@ -25,11 +28,59 @@ const Homepage = () => {
           </div>
           {/* ends here */}
           <img src="/bot.png" alt="" className='bot'/>
+          <div className="chat">
+            <img 
+              src=
+              {
+                typingStatus === "human1"
+                ? "/human1.jpeg"
+                : typingStatus === "human2"
+                ? "/human2.jpeg"
+                : "bot.png"
+              } 
+              alt="" />
+              <TypeAnimation
+              sequence={[
+                // Same substring at the start will only be typed out once, initially
+                "Human:We produce food for Mice",
+                2000,
+                () => {
+                  setTypingStatus("bot");
+                },
+                "Bot:We produce food for Hamsters",
+                2000,
+                () => {
+                  setTypingStatus("human2");
+                },
+                "Human2:We produce food for Guinea Pigs",
+                2000,
+                () => {
+                  setTypingStatus("bot");
+                },
+                "Bot:We produce food for Chinchillas",
+                2000,
+                () => {
+                  setTypingStatus("human1");
+                },
+              ]}
+              wrapper="span"
+              repeat={Infinity}
+              cursor={true}
+              omitDeletionAnimation={true}
+            />
+          </div>
         </div>
-
+      </div>
+      <div className="terms">
+        <img src="/logo.png" alt="" />
+        <div className="links">
+          <Link to="/">Terms of Service</Link>
+          <span>|</span>
+          <Link to="/">Privacy Policy</Link>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Homepage
