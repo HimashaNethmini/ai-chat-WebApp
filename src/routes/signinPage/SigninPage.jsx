ec3bed8ba7033e0b1ad17d706e0361b04a07260c
HimashaNethmini <himashanethmini05@gmail.com>
2024-07-31 10:59:00 +0530

    when click on sigin btn, navigates to sign-up page

--- src/routes/signinPage/SigninPage.jsx
+++ src/routes/signinPage/SigninPage.jsx
@@ -4,7 +4,7 @@ import { SignIn } from '@clerk/clerk-react';
 const SigninPage = () => {
   return (
     <div className='signinPage'>
-      <SignIn path ="/sign-in" />
+      <SignIn path ="/sign-in"  signUpUrl="/sign-up"/> {/* when click on sigin btn, navigates to sign-up page */}
     </div>
   )
 }
