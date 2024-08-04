8175a12da479b389d7d4b7489fefa76c46584581
HimashaNethmini <himashanethmini05@gmail.com>
2024-08-04 22:08:41 +0530

    completed dynamic chatList

---
+++ src/components/chatList/ChatList.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const ChatList = () => {
+  return (
+    <div className='ChatList'>
+        <span className='title'>DASHBOARD</span>
+        <Link to = "/dashboard">Create a new Chat</Link>
+        <Link to = "/">Explore Chat A </Link>
+        <Link to = "/" >Content</Link>
+        <hr />
+        <div className="list">
+            <Link to="/"> My Chat List </Link>
+            <Link to="/"> My Chat List </Link>
+            <Link to="/"> My Chat List </Link>
+            <Link to="/"> My Chat List </Link>
+            <Link to="/"> My Chat List </Link>
+            <Link to="/"> My Chat List </Link>
+            <Link to="/"> My Chat List </Link>
+            <Link to="/"> My Chat List </Link>
+        </div>
+        <hr />
+        <div className="upgrade">
+            <img src="/logo.png" alt="" />
+            <div className="texts">
+                <span>Upgrade to Chat AI Pro</span>
+                <span>Get Access to Unlimited Features</span>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ChatList
