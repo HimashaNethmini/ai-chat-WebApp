f7a7ee2aed6651efb2e21f78e31ecb892dbdd8a6
HimashaNethmini <himashanethmini05@gmail.com>
2024-08-04 21:08:22 +0530

    force redirecting to dashboard

--- src/routes/signinPage/SigninPage.jsx
+++ src/routes/signinPage/SigninPage.jsx
@@ -1,12 +1,17 @@
-import './signinPage.css';
-import { SignIn } from '@clerk/clerk-react';
+import "./signinPage.css";
+import { SignIn } from "@clerk/clerk-react";
 
 const SigninPage = () => {
   return (
-    <div className='signinPage'>
-      <SignIn path ="/sign-in"  signUpUrl="/sign-up"/> {/* when click on sigin btn, navigates to sign-up page */}
+    <div className="signinPage">
+      {/* when click on sigin btn, navigates to sign-up page */}
+      <SignIn
+        path="/sign-in"
+        signUpUrl="/sign-up"
+        forceRedirectUrl="/dashboard"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default SigninPage
+export default SigninPage;
