4fc5460fc8f7295bfc7b9491458728929cbdd445
HimashaNethmini <himashanethmini05@gmail.com>
2024-08-07 12:15:49 +0530

    add scroll effect

--- src/routes/chatPage/ChatPage.jsx
+++ src/routes/chatPage/ChatPage.jsx
@@ -1,6 +1,16 @@
+import { useRef } from 'react'
 import './chatPage.css'
+import { useEffect } from 'react'
 
 const ChatPage = () => {
+
+  const endRef = useRef(null)
+
+  // scroll effect
+  useEffect(()=> {
+    endRef.current.scrollIntoView ({behavior: "smooth" });
+  }, []);
+
   return (
     <div className='ChatPage'>
       <div className="wrapper">
@@ -9,6 +19,14 @@ const ChatPage = () => {
           <div className="message user">User</div>
           <div className="message">AI</div>
           <div className="message user">User</div>
+          <div className="message">AI</div>
+          <div className="message user">User</div>
+          <div className="message">AI</div>
+          <div className="message user">User</div>
+
+          {/* using ref hook */}
+          <div ref = {endRef} />
+
         </div>
       </div>
     </div>
