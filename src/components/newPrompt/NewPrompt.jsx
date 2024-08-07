56351e54f3c392521e6b3ed83ab346b4efe29774
HimashaNethmini <himashanethmini05@gmail.com>
2024-08-07 12:39:08 +0530

    attaching images and in the chat

--- src/components/newPrompt/NewPrompt.jsx
+++ src/components/newPrompt/NewPrompt.jsx
@@ -2,7 +2,21 @@ import './newPrompt.css';
 
 const NewPrompt = () => {
   return (
-    <div className='newPromp'>t</div>
+    
+    // attaching images and in the chat
+    <div className='newPrompt'>
+        <form className="newForm">
+            <label htmlFor="file">
+                <img src="/attachment.png" alt="" />
+            </label>
+            <input id="file" type="file" multiple={false} hidden/>
+            <input type="text" placeholder='Ask anything ....... ' />
+            <button>
+                <img src="/arrow.png" alt="" />
+            </button>
+
+        </form>
+    </div>
   )
 }
 
