5569ac4fadd2e16165a97d7cda28391978484981
HimashaNethmini <himashanethmini05@gmail.com>
2024-08-09 10:20:19 +0530

    mapping with the imagekit credentials

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,18 @@
 import express from "express"
+import ImageKit from "imagekit";
 
 const port = process.env.PORT || 3000;
 const app = express();
 
+//mapping with the imagekit credentials
+const imagekit = new ImageKit({
+    urlEndpoint: process.env.IMAGE_KIT_ENDPOINT,
+    publicKey: process.env.IMAGE_KIT_PUBLIC_KEY,
+    privateKey: process.env.IMAGE_KIT_PRIVATE_KEY,
+})
+
 //endpoint
-app.get("/test",(req,res)=>{
+app.get("/api/upload",(req,res)=>{
     res.send("it works !")
 })
 
