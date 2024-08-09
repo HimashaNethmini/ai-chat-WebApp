27f551db21d558e5afcffe6f687174178deb6738
HimashaNethmini <himashanethmini05@gmail.com>
2024-08-09 09:54:17 +0530

    added endpoint for testing

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,13 @@
-console.log("hello");
+import express from "express"
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+//endpoint
+app.get("/test",(req,res)=>{
+    res.send("it works !")
+})
+
+app.listen(port, ()=>{
+    console.log("App is running on", port)
+})
