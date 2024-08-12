5cc444bb628022c0cdf07db755435d10e24de675
HimashaNethmini <himashanethmini05@gmail.com>
2024-08-12 09:33:17 +0530

    added functions

--- frontend/src/components/upload/Upload.jsx
+++ frontend/src/components/upload/Upload.jsx
@@ -2,43 +2,62 @@ const urlEndpoint = import.meta.env.VITE_IMAGE_KIT_ENDPOINT;
 const publicKey = import.meta.env.VITE_IMAGE_KIT_PUBLIC_KEY;
 
 const authenticator = async () => {
-    try {
-        const response = await fetch('http://localhost:3000/api/upload');
+  try {
+    const response = await fetch("http://localhost:3000/api/upload");
 
-        //if not aunthenticated
-        if (!response.ok) {
-            const errorText = await response.text();
-            throw new Error(`Request failed with status ${response.status}: ${errorText}`);
-        }
-
-        //if authenticated, send token and signature to upload images
-        const data = await response.json();
-        const { signature, expire, token } = data;
-        return { signature, expire, token };
-    } catch (error) {
-        throw new Error(`Authentication request failed: ${error.message}`);
+    //if not aunthenticated
+    if (!response.ok) {
+      const errorText = await response.text();
+      throw new Error(
+        `Request failed with status ${response.status}: ${errorText}`
+      );
     }
+
+    //if authenticated, send token and signature to upload images
+    const data = await response.json();
+    const { signature, expire, token } = data;
+    return { signature, expire, token };
+  } catch (error) {
+    throw new Error(`Authentication request failed: ${error.message}`);
+  }
 };
 
 const Upload = () => {
+  const onError = (err) => {
+    console.log("Error", err);
+  };
+
+  const onSuccess = (res) => {
+    console.log("Success", res);
+  };
+
+  const onUploadProgress = (progress) => {
+    console.log("Progress", progress);
+  };
+
+  const onUploadStart = (evt) => {
+    console.log("Start", evt);
+  };
+
   return (
     <div className="Upload">
-        <IKContent 
-            urlEndpoint={urlEndpoint}
-            publicKey={publicKey}
-            authenticator={authenticator}
-        >
-            {/* upload component */}
-            <IKUpload
-                fileName = "test-upload.png"
-                onError={onError}
-                onSuccess={onSuccess}>
-
-            </IKUpload>
-
-        </IKContent>
+      <IKContent
+        urlEndpoint={urlEndpoint}
+        publicKey={publicKey}
+        authenticator={authenticator}
+      >
+        {/* upload component */}
+        <IKUpload
+          fileName="test-upload.png"
+          onError={onError}
+          onSuccess={onSuccess}
+          useUniqueFileName={true}
+          onUploadProgress={onUploadProgress}
+          onUploadStart={onUploadStart}
+        ></IKUpload>
+      </IKContent>
     </div>
-  )
-}
+  );
+};
 
-export default Upload
+export default Upload;
