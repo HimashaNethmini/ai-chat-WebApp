15bcee3c4597f46e0921470cbe7c53646a3b9f68
HimashaNethmini <himashanethmini05@gmail.com>
2024-08-12 09:22:49 +0530

    if not aunthenticated

--- frontend/src/components/upload/Upload.jsx
+++ frontend/src/components/upload/Upload.jsx
@@ -5,6 +5,7 @@ const authenticator = async () => {
     try {
         const response = await fetch('http://localhost:3000/api/upload');
 
+        //if not aunthenticated
         if (!response.ok) {
             const errorText = await response.text();
             throw new Error(`Request failed with status ${response.status}: ${errorText}`);
