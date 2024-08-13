be62488bb98e01bddcae718fbd2e433e9c88d498
HimashaNethmini <himashanethmini05@gmail.com>
2024-08-13 22:28:34 +0530

    uploading the image

--- frontend/src/components/newPrompt/NewPrompt.jsx
+++ frontend/src/components/newPrompt/NewPrompt.jsx
@@ -2,11 +2,18 @@ import Upload from "../upload/Upload";
 import "./newPrompt.css";
 
 const NewPrompt = () => {
+
+  const [img, setImg ] = useState({
+    isLoading:false,
+    error:"",
+    dbData: {}
+  })
+
   return (
     <>
       {/* attaching images and in the chat */}
       <form className="newForm">
-        <Upload />
+        <Upload setImg={setImg}/>
         <input id="file" type="file" multiple={false} hidden />
         <input type="text" placeholder="Ask anything ....... " />
         <button>
