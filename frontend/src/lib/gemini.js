4808dd9dfc7b1df8a58cf3c90187ba11795b7593
HimashaNethmini <himashanethmini05@gmail.com>
2024-08-15 09:48:32 +0530

    added safety settings to limit the responses

--- frontend/src/lib/gemini.js
+++ frontend/src/lib/gemini.js
@@ -1,7 +1,21 @@
 //initializing the model
-const { GoogleGenerativeAI } = require("@google/generative-ai");
+import { GoogleGenerativeAI, HarmBlockThreshold, HarmCategory } from "@google/generative-ai";
+
+//added safety settings to limit the responses
+const safetySettig = [
+    {
+        category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+        threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+    },
+    {
+        category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+        threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+    }
+]
 
 // Access your API key as an environment variable 
 const genAI = new GoogleGenerativeAI(process.env.VITE_GEMINI_PUBLIC_API_KEY);
 
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
+
+export default model;
