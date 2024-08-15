4ac12fa35f5e15318518cb94d43124b9ce6f79e0
HimashaNethmini <himashanethmini05@gmail.com>
2024-08-15 09:54:52 +0530

    make the first request

--- frontend/src/components/newPrompt/NewPrompt.jsx
+++ frontend/src/components/newPrompt/NewPrompt.jsx
@@ -2,6 +2,7 @@ import { IKImage } from "imagekitio-react";
 import Upload from "../upload/Upload";
 import "./newPrompt.css";
 import { useEffect, useRef } from "react";
+import model from "../../lib/gemini";
 
 const NewPrompt = () => {
 
@@ -17,6 +18,16 @@ const NewPrompt = () => {
   useEffect(() => {
     endRef.current.scrollIntoView({ behavior: "smooth" })
   }, []);
+
+  //make the first request
+  const add = async () => {
+      const prompt = "Write a story about an AI and magic"
+    
+      const result = await model.generateContent(prompt);
+      const response = await result.response;
+      const text = response.text();
+      console.log(text);
+    };
 
   return (
     <>
