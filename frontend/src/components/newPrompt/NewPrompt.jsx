6c5bd879cce169b8b397fe5bb12d3dd57ecf90c2
HimashaNethmini <himashanethmini05@gmail.com>
2024-08-16 10:22:11 +0530

    make the first request

--- frontend/src/components/newPrompt/NewPrompt.jsx
+++ frontend/src/components/newPrompt/NewPrompt.jsx
@@ -6,57 +6,75 @@ import model from "../../lib/gemini";
 
 const NewPrompt = () => {
   //to dynamically chat with AI
-  const [ question, setQuestions] = useState("");
-  const [ answer, setAnswer ] = useState("");
+  const [question, setQuestions] = useState("");
+  const [answer, setAnswer] = useState("");
 
-  const [img, setImg ] = useState({
-    isLoading:false,
-    error:"",
-    dbData: {}
+  const [img, setImg] = useState({
+    isLoading: false,
+    error: "",
+    dbData: {},
+    aiData: {},
   });
 
   //scrolling effect
-  // const endRef = useRef(null);
+  const endRef = useRef(null);
+  const formRef = useRef(null);
 
-  // useEffect(() => {
-  //   endRef.current.scrollIntoView({ behavior: "smooth" })
-  // }, []);
+  useEffect(() => {
+    endRef.current.scrollIntoView({ behavior: "smooth" });
+  }, [data, question, answer, img.dbData]);
 
   //make the first request
-  const add = async (text) => {
-      setQuestions(text)
-    
-      const result = await model.generateContent(prompt);
-      const response = await result.response;
-      setAnswer(response.text ())
-    };
+  const add = async (text, isInitial) => {
+    if (!isInitial) setQuestions(text);
 
-    //handle ai responses
-    const handleSubmit = async (e) => {
-      e.preventDefault()
+    try {
+      const result = await chat.sendMessageStream(
+        Object.entries(img.aiData).length ? [img.aiData, text] : [text]
+      );
+      let accumulatedText = "";
+      for await (const chunk of result.stream) {
+        const chunkText = chunk.text();
+        console.log(chunkText);
+        accumulatedText += chunkText;
+        setAnswer(accumulatedText);
+      }
 
-      const text = e.target.text.value;
-      if (!text) return;
-
-      add(text)
+      mutation.mutate();
+    } catch (err) {
+      console.log(err);
     }
+  };
+
+  //handle ai responses
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const text = e.target.text.value;
+    if (!text) return;
+
+    add(text, false);
+  };
 
   return (
     <>
-    {/* add new chat */}
-    {/* Loading the image */}
-    {img.isLoading && <div className="">Loading ...</div>}
-    {img.dbData?.filePath && (
-      <IKImage
-        urlEndpoint={import.meta.env.VITE_IMAGE_KIT_ENDPOINT}
-        path={img.dbData?.filePath}
-        width="380"
-        transformation={{ width:380 }} // resize the image
-      />
-    )}
+      {/* add new chat */}
+      {/* Loading the image */}
+      {img.isLoading && <div className="">Loading ...</div>}
+      {img.dbData?.filePath && (
+        <IKImage
+          urlEndpoint={import.meta.env.VITE_IMAGE_KIT_ENDPOINT}
+          path={img.dbData?.filePath}
+          width="380"
+          transformation={{ width: 380 }} // resize the image
+        />
+      )}
+      {question && <div className="message user"> {question} </div>}
+      {answer && <div className="message">{answer}</div>}
+
       {/* attaching images and in the chat */}
-      <form className="newForm" onSubmit={handleSubmit}>
-        <Upload setImg={setImg}/>
+      <form className="newForm" onSubmit={handleSubmit} ref={formRef}>
+        <Upload setImg={setImg} />
         <input id="file" type="file" multiple={false} hidden />
         <input type="text" name="text" placeholder="Ask anything ....... " />
         <button>
