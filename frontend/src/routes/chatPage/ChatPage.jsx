fa311ea6362338fa773f1bf263be8c9d2933c0b6
HimashaNethmini <himashanethmini05@gmail.com>
2024-08-29 21:50:42 +0530

    updated

--- frontend/src/routes/chatPage/ChatPage.jsx
+++ frontend/src/routes/chatPage/ChatPage.jsx
@@ -1,38 +1,66 @@
-import { useRef } from 'react'
-import './chatPage.css'
-import { useEffect } from 'react'
-import NewPrompt from '../../components/newPrompt/NewPrompt'
+import { useRef } from "react";
+import "./chatPage.css";
+import { useQuery } from "react";
+import NewPrompt from "../../components/newPrompt/NewPrompt";
 
 const ChatPage = () => {
+  const path = useLocation().pathname;
+  const chatId = path.split("/").pop();
 
-  const endRef = useRef(null)
+  const { isPending, error, data } = useQuery({
+    queryKey: ["chat", chatId],
+    queryFn: () =>
+      fetch(`${import.meta.env.VITE_API_URL}/api/chats/${chatId}`, {
+        credentials: "include",
+      }).then((res) => res.json()),
+  });
 
-  // scroll effect
-  useEffect(()=> {
-    endRef.current.scrollIntoView ({behavior: "smooth" });
-  }, []);
+  console.log(data);
+
+  // const endRef = useRef(null)
+
+  // // scroll effect
+  // useEffect(()=> {
+  //   endRef.current.scrollIntoView ({behavior: "smooth" });
+  // }, []);
 
   return (
-    <div className='ChatPage'>
+    <div className="ChatPage">
       <div className="wrapper">
         <div className="chat">
-          <div className="message">AI</div>
-          <div className="message user">User</div>
-          <div className="message">AI</div>
-          <div className="message user">User</div>
-          <div className="message">AI</div>
-          <div className="message user">User</div>
-          <div className="message">AI</div>
-          <div className="message user">User</div>
-          <NewPrompt />
+          {isPending
+            ? "Loading..."
+            : error
+            ? "Something went wrong!"
+            : data?.history?.map((message, i) => (
+                <>
+                  {message.img && (
+                    <IKImage
+                      urlEndpoint={import.meta.env.VITE_IMAGE_KIT_ENDPOINT}
+                      path={message.img}
+                      height="300"
+                      width="400"
+                      transformation={[{ height: 300, width: 400 }]}
+                      loading="lazy"
+                      lqip={{ active: true, quality: 20 }}
+                    />
+                  )}
+                  <div
+                    className={
+                      message.role === "user" ? "message user" : "message"
+                    }
+                    key={i}
+                  >
+                    <Markdown>{message.parts[0].text}</Markdown>
+                  </div>
+                </>
+              ))}
 
-          {/* using ref hook */}
-          <div ref = {endRef} />
-
+          {data && <NewPrompt data={data} />}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ChatPage
+export default ChatPage;
