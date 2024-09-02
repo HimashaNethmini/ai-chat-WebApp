beb3796e2ffc11da464573452334cadb130f192b
HimashaNethmini <himashanethmini05@gmail.com>
2024-09-02 12:47:31 +0530

    enable react-query in app

--- frontend/src/layouts/rootLayout/RootLayout.jsx
+++ frontend/src/layouts/rootLayout/RootLayout.jsx
@@ -1,39 +1,44 @@
-import './rootLayout.css'
+import "./rootLayout.css";
 import { Link } from "react-router-dom";
-import { Outlet } from 'react-router-dom';
-import { ClerkProvider, SignedIn, UserButton } from '@clerk/clerk-react'
+import { Outlet } from "react-router-dom";
+import { ClerkProvider, SignedIn, UserButton } from "@clerk/clerk-react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 // Import the publishable key
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
 }
 
+//import react-query
+const queryClient = new QueryClient();
+
 // enabling client side routing
 const RootLayout = () => {
   return (
-
     //add <ClerkProvider> to my app
     <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-      <div className="rootLayout">
-        <header>
-          <Link to="/dashboard" className='logo'>
-            <img src="/logo.png" alt="" />
-            <span>CHAT AI</span>
-          </Link>
+      <QueryClientProvider client={queryClient}>
+        <div className="rootLayout">
+          <header>
+            <Link to="/dashboard" className="logo">
+              <img src="/logo.png" alt="" />
+              <span>CHAT AI</span>
+            </Link>
 
-          <div className="user">
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-          </div>
-        </header>  
+            <div className="user">
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </div>
+          </header>
 
-        <main>
-          <Outlet />
-        </main>
-      </div>
+          <main>
+            <Outlet />
+          </main>
+        </div>
+      </QueryClientProvider>
     </ClerkProvider>
   );
 };
